1199f564d34ec0911532025eaddd229652675160
Jimpei Yamamoto <zinpei104@gmail.com>
2023-02-16 19:02:55 +0900

    企業目一覧画面_

---
+++ PR-GOBOU/PR-GOBOU/Setting/FollowCompanyView.swift
@@ -0,0 +1,78 @@
+//
+//  FollowCompanyView.swift
+//  PR-GOBOU
+//
+//  Created by 水代謝システム工学研究室 on 2023/02/16.
+//
+
+import SwiftUI
+
+struct FollowCompanyView: View {
+
+    let companyIds: [Int] = [
+        1,
+        2,
+    ]
+    let companyNames: [String] = [
+        "株式会社PR TIMES",
+        "株式会社エイトノット"
+    ]
+    
+    @State private var isCheckeds: [Bool] = [false, false]
+    
+    var body: some View {
+        List {
+            ForEach(Array(companyIds.enumerated()), id: \.element) { index, _ in
+                HStack {
+                    Button {
+                        isCheckeds[index] = !isCheckeds[index]
+                        UIImpactFeedbackGenerator(style: .medium)
+                            .impactOccurred()
+                        saveFollowCompanyIds()
+                    } label: {
+                        if isCheckeds[index] {
+                            Image(systemName: "checkmark.square.fill")
+                                .foregroundColor(.blue)
+                        } else {
+                            Image(systemName: "square")
+                        }
+                    }
+                    Text(companyNames[index])
+                }
+            }
+        }
+        .listStyle(.plain)
+        .navigationTitle(Text("フォローする企業を選択してください"))
+        .navigationBarItems(
+            leading:NavigationLink(
+                destination: {EmptyView()},
+                label: {Text("完了")}
+            )
+        )
+    }
+    
+    private func saveFollowCompanyIds() {
+        var saveIds:[Int] = []
+        
+        for (index, companyId) in self.companyIds.enumerated() {
+            if isCheckeds[index] {
+                saveIds.append(companyId)
+            }
+        }
+        UserDefaults.standard.set(saveIds, forKey: "followCompanyIds")
+    }
+    
+    private func loadFollowCompanyIds() {
+        if UserDefaults.standard.object(forKey: "followCompanyIds") != nil {
+            let saveIds = UserDefaults.standard.array(forKey: "followCompanyIds")
+            for (index, companyId) in companyIds.enumerated() {
+            }
+        }
+    }
+}
+
+struct FollowCompanyView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowCompanyView()
+    }
+}
