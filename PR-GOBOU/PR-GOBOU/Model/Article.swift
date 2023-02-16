cfce6005ee0916f961bf2745d0124a4c8b4bcd45
Jimpei Yamamoto <zinpei104@gmail.com>
2023-02-16 15:26:47 +0900

    Modelの追加

---
+++ PR-GOBOU/PR-GOBOU/Model/Article.swift
@@ -0,0 +1,28 @@
+//
+//  Article.swift
+//  PR-GOBOU
+//
+//  Created by 水代謝システム工学研究室 on 2023/02/16.
+//
+
+import Foundation
+
+struct ArticleInfo {
+    let companyName: String
+    let companyId: Int
+    let releaseId: Int
+    let title: String
+    let subtitle: String
+    let url: String
+    let leadParagraph: Int
+    let body: String
+    let mainImage: String
+    let mainImageFastly: String
+    let mainCategoryId: Int
+    let mainCategoryName: String
+    let subCategoryId: Int
+    let subCategoryName: String
+    let prType: String
+    let createdAt: String
+}
+
---
+++ PR-GOBOU/PR-GOBOU/Model/Reputation.swift
@@ -0,0 +1,14 @@
+//
+//  Pvuu.swift
+//  PR-GOBOU
+//
+//  Created by 水代謝システム工学研究室 on 2023/02/16.
+//
+
+import Foundation
+
+struct Reputation {
+    let uniqueUser: Int
+    let pageView: Int
+    let like: Int
+}
